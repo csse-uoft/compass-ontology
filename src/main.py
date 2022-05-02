68172c50f201d53c32dcd048acd233521fe7e4b5
LesterLyu <lvds2000@gmail.com>
2022-05-02 12:35:35 -0400

    Fix ontology metadata.

--- src/main.py
+++ src/main.py
@@ -29,22 +29,30 @@ sur_onto.set_location('http://ontology.eil.utoronto.ca/tove/survey.owl')
 
 compass_onto.imported_ontologies = [cids_onto, ic_onto, schema_onto, act_50871, sur_onto, city_50872]
 
+# Ontology Metadata Namespaces
+vann = default_world.get_namespace("http://purl.org/vocab/vann/")
+dc = default_world.get_namespace("http://purl.org/dc/elements/1.1/")
+cc = default_world.get_namespace("http://creativecommons.org/ns#")
+dcterms = default_world.get_namespace("http://purl.org/dc/terms/")
+owl = default_world.get_namespace("http://www.w3.org/2002/07/owl#")
+
 # Ontology Metadata
-compass_onto.metadata.preferredNamespacePrefix.append('cp')
-compass_onto.metadata.preferredNamespaceUri.append('http://helpseeker.co/compass#')
-compass_onto.metadata.title.append('Compass Ontology')
-compass_onto.metadata.description.append("""
+vann.preferredNamespacePrefix[compass_onto.metadata] = ['cp']
+vann.preferredNamespaceUri[compass_onto.metadata] = ['http://helpseeker.co/compass#']
+dcterms.title[compass_onto.metadata] = ['Compass Ontology']
+dcterms.description[compass_onto.metadata] = ["""
 The Compass ontology contains
 - Compass Project Client Ontology
 - Compass Project Service Ontology
-- Compass Project Need Ontology""")
-compass_onto.metadata.issued.append('January 27, 2022')
-compass_onto.metadata.modified.append(date.today().strftime("%B %d, %Y"))
-compass_onto.metadata.versionInfo.append("""
-Version 0 January 27, 2022: Initial creation.""")
-compass_onto.metadata.license.append('http://creativecommons.org/licenses/by/3.0/')
-compass_onto.metadata.creator.append('Mark S. Fox, msf@eil.utoronto.ca; Daniela Rosu, drosu@cs.toronto.edu; '
-                                     'Bart Gajderowicz, bartg@mie.utoronto.ca; Dishu Lyu, lvds2000@gmail.com')
+- Compass Project Need Ontology"""]
+
+dcterms.issued[compass_onto.metadata] = ['January 27, 2022']
+dcterms.modified[compass_onto.metadata] = [date.today().strftime("%B %d, %Y")]
+owl.versionInfo[compass_onto.metadata] = ["""
+Version 0 January 27, 2022: Initial creation."""]
+cc.license[compass_onto.metadata] = ['http://creativecommons.org/licenses/by/3.0/']
+dcterms.creator[compass_onto.metadata] = ['Mark S. Fox, msf@eil.utoronto.ca; Daniela Rosu, drosu@cs.toronto.edu; '
+                                     'Bart Gajderowicz, bartg@mie.utoronto.ca; Dishu Lyu, lvds2000@gmail.com']
 
 
 print('--------------------Loaded-------------------')
