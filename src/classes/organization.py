82023b7a6de34678f03d93787cb0a05716116bd2
LesterLyu <lvds2000@gmail.com>
2022-07-11 12:09:55 -0400

    Add ic:hasEmail to both organization and program.

--- src/classes/organization.py
+++ src/classes/organization.py
@@ -1,5 +1,5 @@
 from owlready2 import *
-from src.namespaces import compass, cids, schema, tove_organization, dcterms
+from src.namespaces import compass, cids, schema, tove_organization, dcterms, ic
 from src.utils import get_class
 
 
@@ -7,7 +7,8 @@ class Organization(cids.Organization):
     label = 'Organization'
     is_a = [
         compass.hasFunding.only(get_class('Funding')),
-        compass.hasLanguage.only(get_class('CL-Language'))
+        compass.hasLanguage.only(get_class('CL-Language')),
+        ic.hasEmail.only(str),
     ]
 
 
@@ -15,4 +16,6 @@ dcterms.description[Organization] = """
 The Compass Organization extends cids:Organization with funding information.
 
 • hasFunding: links to instances of Funding that specify the funding received by the organization.
+• hasLanguage: Language for the Organization.
+• hasEmail: Email for the Organization.
 """
--- src/classes/program.py
+++ src/classes/program.py
@@ -6,7 +6,8 @@ from src.utils import get_class
 class Program(cids.Program):
     label = 'Program'
     is_a = [
-        ic.hasAddress.only(ic.Address)
+        ic.hasAddress.only(ic.Address),
+        ic.hasEmail.only(str),
     ]
 
 
@@ -15,4 +16,5 @@ The Program class extends cids:Program with an address field. This allows an org
 For example, a “health” service can set up multiple offices closer to its clients.
 
 • hasAddress: Address for the program.
+• hasEmail: Email for the program.
 """
